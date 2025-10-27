e068d1c9e8b73794b615eaaf305a6e1edf9433db
namdeptraivcd <namhoang4d@gmail.com>
2025-10-27 19:37:36 +0700

    Test topic/class-members successfully

---
+++ lab3/src/DVD_test.java
@@ -0,0 +1,15 @@
+package lab3.src;
+
+public class DVD_test {
+    
+    public static void main(String[] args) {
+        DVD dvd1 = new DVD("kimi");
+        DVD dvd2 = new DVD("nawa");
+
+        System.out.println(dvd1.getId());
+        System.out.println(dvd2.getId());
+        
+        System.out.println(dvd1.getNumberDVD());
+        System.out.println(dvd2.getNumberDVD());
+    }
+}
