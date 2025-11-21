64953b038227df9c9611e12d9fa9f198f0355d64
namdeptraivcd <namhoang4d@gmail.com>
2025-11-21 11:57:00 +0700

    Finish task 8 Update the Store class to work with Media

---
+++ lab4/src/aims/media/Store.java
@@ -0,0 +1,51 @@
+package lab4.src.aims.media;
+import java.util.ArrayList;
+public class Store {
+
+
+    private ArrayList<Media> itemsInStore = new ArrayList<Media>();
+
+
+    public Store(){}
+
+    public void add_DVD(Media media) {
+        if (media != null) {
+            itemsInStore.add(media);
+            System.out.println("Added media: " + media.getTitle() + " to the store.");
+        } else {
+            System.out.println("Cannot add null item to the store");
+        }
+    }
+
+    public void remove_DVD(Media media) {
+
+        if (media == null) {
+            System.out.println("Cannot remove null item from store");
+            return;
+            }
+        
+        if (itemsInStore.remove(media)) {
+            System.out.println("Remove media: " + media.getTitle() + " successfully from the Store");
+            }
+
+        else {
+            System.out.println("Media " + media.getTitle() + " not found in store.");
+        }
+    }
+
+    public void printStore(){
+        System.out.println("\n**********************STORE ITEMS**********************");
+        if (itemsInStore.isEmpty()) {
+            System.out.println("Store is currently empty.");
+        } else {
+            for (int i = 0; i < itemsInStore.size(); i++) {
+                Media item = itemsInStore.get(i);
+                System.out.println((i + 1) + ". " + item.toString());
+            }
+        }
+        System.out.println("*******************************************************");
+    }
+
+}
+
+
