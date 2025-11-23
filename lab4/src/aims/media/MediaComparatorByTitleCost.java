0290a719d653b1213313b878345de31edce8af60
namdeptraivcd <namhoang4d@gmail.com>
2025-11-24 00:49:03 +0700

    finish task12 Sort media in the cart

--- lab4/src/aims/media/MediaComparatorByTitleCost.java
+++ lab4/src/aims/media/MediaComparatorByTitleCost.java
@@ -3,5 +3,11 @@ package lab4.src.aims.media;
 import java.util.Comparator;
 
 public class MediaComparatorByTitleCost implements Comparator<Media> {
-    
+    @Override
+    public int compare(Media media1, Media media2) {
+        return Comparator
+                .comparing(Media::getTitle)
+                .thenComparing(Media::getCost, Comparator.reverseOrder())
+                .compare(media1, media2);
+    }
 }
